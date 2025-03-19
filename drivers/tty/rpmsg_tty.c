6e4949f86e3772346887856cb3089a21c208e88e
Andre Heinemans <andre.heinemans@nxp.com>
2025-03-19 11:49:45 +0100

    drivers: tty: rpmsg-tty original and new both available
    rpmsg_tty compatible to NuttX is moved to rpmsg_tty2
    
    Channel names that matches exactly "rpmsg-tty" will get the original
    rpmsg_tty driver
    
    Channel names that starts with "rpmsg-tty" as prefix will get the
    Nuttx compatible rpmsg_tty2 driver

--- drivers/tty/rpmsg_tty.c
+++ drivers/tty/rpmsg_tty.c
@@ -1,373 +1,288 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2017 Pinecone Inc.
+ * Copyright (C) 2021 STMicroelectronics - All Rights Reserved
+ *
+ * The rpmsg tty driver implements serial communication on the RPMsg bus to makes
+ * possible for user-space programs to send and receive rpmsg messages as a standard
+ * tty protocol.
+ *
+ * The remote processor can instantiate a new tty by requesting a "rpmsg-tty" RPMsg service.
+ * The "rpmsg-tty" service is directly used for data exchange. No flow control is implemented yet.
  */
 
+#define pr_fmt(fmt)		KBUILD_MODNAME ": " fmt
+
 #include <linux/module.h>
-#include <linux/of.h>
 #include <linux/rpmsg.h>
+#include <linux/slab.h>
 #include <linux/tty.h>
 #include <linux/tty_flip.h>
-#include <linux/workqueue.h>
 
-#include <generated/uapi/linux/version.h>
+#define RPMSG_TTY_NAME	"ttyRPMSG"
+#define MAX_TTY_RPMSG	32
 
-#define RPMSG_TTY_WRITE		0
-#define RPMSG_TTY_WAKEUP	1
+static DEFINE_IDR(tty_idr);	/* tty instance id */
+static DEFINE_MUTEX(idr_lock);	/* protects tty_idr */
 
-struct rpmsg_tty_header {
-	u32			command:31;
-	u32			response:1;
-	s32			result;
-	u64			cookie;
-} __packed;
+static struct tty_driver *rpmsg_tty_driver;
 
-struct rpmsg_tty_write {
-	struct rpmsg_tty_header	header;
-	u32			count;
-	u32			resolved;
-	char			data[0];
-} __packed;
-
-#define rpmsg_tty_wakeup	rpmsg_tty_header
-
-struct rpmsg_tty_cookie {
-	struct completion	done;
-	s32			result;
+struct rpmsg_tty_port {
+	struct tty_port		port;	 /* TTY port data */
+	int			id;	 /* TTY rpmsg index */
+	struct rpmsg_device	*rpdev;	 /* rpmsg device */
 };
 
-struct tty_rpmsg_port {
-	struct tty_port		port;
-	struct work_struct	work;
-	struct tty_driver	*driver;
-	int			xmit_size;
-};
-
-static struct tty_rpmsg_port *to_tty_rpmsg_port(struct tty_port *p)
+static int rpmsg_tty_cb(struct rpmsg_device *rpdev, void *data, int len, void *priv, u32 src)
 {
-	return container_of(p, struct tty_rpmsg_port, port);
+	struct rpmsg_tty_port *cport = dev_get_drvdata(&rpdev->dev);
+	int copied;
+
+	if (!len)
+		return -EINVAL;
+	copied = tty_insert_flip_string(&cport->port, data, len);
+	if (copied != len)
+		dev_err_ratelimited(&rpdev->dev, "Trunc buffer: available space is %d\n", copied);
+	tty_flip_buffer_push(&cport->port);
+
+	return 0;
 }
 
-static int tty_rpmsg_port_activate(struct tty_port *p, struct tty_struct *tty)
+static int rpmsg_tty_install(struct tty_driver *driver, struct tty_struct *tty)
 {
-	struct tty_rpmsg_port *port = to_tty_rpmsg_port(p);
+	struct rpmsg_tty_port *cport = idr_find(&tty_idr, tty->index);
+	struct tty_port *port;
 
-	port->xmit_size = 0;
-	return tty_port_alloc_xmit_buf(p);
+	tty->driver_data = cport;
+
+	port = tty_port_get(&cport->port);
+	return tty_port_install(port, driver, tty);
 }
 
-static void tty_rpmsg_port_shutdown(struct tty_port *p)
+static void rpmsg_tty_cleanup(struct tty_struct *tty)
 {
-	tty_port_free_xmit_buf(p);
+	tty_port_put(tty->port);
 }
 
-static void tty_rpmsg_port_destruct(struct tty_port *p)
+static int rpmsg_tty_open(struct tty_struct *tty, struct file *filp)
 {
-	/* don't need free manually since it come from devm_kzalloc */
-}
-
-static const struct tty_port_operations tty_rpmsg_port_ops = {
-	.activate = tty_rpmsg_port_activate,
-	.shutdown = tty_rpmsg_port_shutdown,
-	.destruct = tty_rpmsg_port_destruct,
-};
-
-static int tty_rpmsg_open(struct tty_struct *tty, struct file *filp)
-{
-	tty_flip_buffer_push(tty->port);
 	return tty_port_open(tty->port, tty, filp);
 }
 
-static void tty_rpmsg_close(struct tty_struct *tty, struct file *filp)
+static void rpmsg_tty_close(struct tty_struct *tty, struct file *filp)
 {
-	tty_port_close(tty->port, tty, filp);
+	return tty_port_close(tty->port, tty, filp);
 }
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 14, 0)
-static int tty_rpmsg_write_room(struct tty_struct *tty)
-#else
-static unsigned int tty_rpmsg_write_room(struct tty_struct *tty)
-#endif
+static ssize_t rpmsg_tty_write(struct tty_struct *tty, const u8 *buf,
+			       size_t len)
 {
-	struct tty_port *p = tty->port;
-	struct tty_rpmsg_port *port = to_tty_rpmsg_port(p);
-	struct rpmsg_device *rpdev = dev_get_drvdata(tty->dev);
-	int space = rpmsg_get_tx_buffer_size(rpdev->ept);
-
-	space -= sizeof(struct rpmsg_tty_write);
-	if (space > PAGE_SIZE)
-		space = PAGE_SIZE;
-
-	mutex_lock(&p->buf_mutex);
-	space -= port->xmit_size;
-	mutex_unlock(&p->buf_mutex);
-
-	return space < 0 ? 0 : space;
-}
-
-static int tty_rpmsg_do_write(struct tty_struct *tty,
-			      const unsigned char *buf, int count)
-{
-	struct rpmsg_device *rpdev = dev_get_drvdata(tty->dev);
-	struct rpmsg_tty_cookie cookie;
-	struct rpmsg_tty_write *msg;
-	unsigned int max;
+	struct rpmsg_tty_port *cport = tty->driver_data;
+	struct rpmsg_device *rpdev;
+	int msg_max_size, msg_size;
 	int ret;
 
-	msg = rpmsg_get_tx_payload_buffer(rpdev->ept, &max, true);
-	if (IS_ERR(msg))
-		return PTR_ERR(msg);
+	rpdev = cport->rpdev;
 
-	if (sizeof(*msg) + count > max)
-		count = max - sizeof(*msg);
+	msg_max_size = rpmsg_get_mtu(rpdev->ept);
+	if (msg_max_size < 0)
+		return msg_max_size;
 
-	memset(msg, 0, sizeof(*msg));
+	msg_size = min_t(unsigned int, len, msg_max_size);
 
-	msg->header.command = RPMSG_TTY_WRITE;
-	msg->header.result  = -ENXIO;
-	msg->header.cookie  = (uintptr_t)&cookie;
-
-	msg->count = count;
-	memcpy(msg->data, buf, count);
-
-	memset(&cookie, 0, sizeof(cookie));
-	init_completion(&cookie.done);
-
-	ret = rpmsg_send_nocopy(rpdev->ept, msg, sizeof(*msg) + count);
-	if (ret < 0) {
-		rpmsg_release_tx_buffer(rpdev->ept, msg);
+	/*
+	 * Use rpmsg_trysend instead of rpmsg_send to send the message so the caller is not
+	 * hung until a rpmsg buffer is available. In such case rpmsg_trysend returns -ENOMEM.
+	 */
+	ret = rpmsg_trysend(rpdev->ept, (void *)buf, msg_size);
+	if (ret) {
+		dev_dbg_ratelimited(&rpdev->dev, "rpmsg_send failed: %d\n", ret);
 		return ret;
 	}
 
-	wait_for_completion(&cookie.done);
-	return cookie.result;
+	return msg_size;
 }
 
-static ssize_t tty_rpmsg_write(struct tty_struct *tty,
-			   const unsigned char *buf, size_t count)
+static unsigned int rpmsg_tty_write_room(struct tty_struct *tty)
 {
-	struct tty_port *p = tty->port;
-	struct tty_rpmsg_port *port = to_tty_rpmsg_port(p);
-	int space, written;
+	struct rpmsg_tty_port *cport = tty->driver_data;
+	int size;
 
-	space = tty_rpmsg_write_room(tty);
-	if (count > space)
-		count = space;
+	size = rpmsg_get_mtu(cport->rpdev->ept);
+	if (size < 0)
+		return 0;
 
-	written = count;
-
-	mutex_lock(&p->buf_mutex);
-	if (port->xmit_size) {
-		memcpy(p->xmit_buf + port->xmit_size, buf, count);
-		port->xmit_size += count;
-
-		buf = p->xmit_buf;
-		count = port->xmit_size;
-	}
-
-	if (count) {
-		int ret = tty_rpmsg_do_write(tty, buf, count);
-
-		if (ret > 0) {
-			memmove(p->xmit_buf, buf + ret, count - ret);
-			port->xmit_size = count - ret;
-		} else if (p->xmit_buf != buf) {
-			memcpy(p->xmit_buf, buf, count);
-			port->xmit_size = count;
-		}
-	}
-	mutex_unlock(&p->buf_mutex);
-
-	return written;
+	return size;
 }
 
-static void tty_rpmsg_write_work(struct work_struct *work)
-{
-	struct tty_rpmsg_port *port =
-		container_of(work, struct tty_rpmsg_port, work);
-	struct tty_struct *tty = tty_port_tty_get(&port->port);
-
-	if (tty) {
-		tty_rpmsg_write(tty, NULL, 0);
-		tty_kref_put(tty);
-	}
-
-	tty_port_tty_wakeup(&port->port);
-}
-
-static void tty_rpmsg_hangup(struct tty_struct *tty)
+static void rpmsg_tty_hangup(struct tty_struct *tty)
 {
 	tty_port_hangup(tty->port);
 }
 
-static const struct tty_operations tty_rpmsg_ops = {
-	.open = tty_rpmsg_open,
-	.close = tty_rpmsg_close,
-	.write = tty_rpmsg_write,
-	.write_room = tty_rpmsg_write_room,
-	.hangup = tty_rpmsg_hangup,
+static const struct tty_operations rpmsg_tty_ops = {
+	.install	= rpmsg_tty_install,
+	.open		= rpmsg_tty_open,
+	.close		= rpmsg_tty_close,
+	.write		= rpmsg_tty_write,
+	.write_room	= rpmsg_tty_write_room,
+	.hangup		= rpmsg_tty_hangup,
+	.cleanup	= rpmsg_tty_cleanup,
 };
+
+static struct rpmsg_tty_port *rpmsg_tty_alloc_cport(void)
+{
+	struct rpmsg_tty_port *cport;
+	int ret;
+
+	cport = kzalloc(sizeof(*cport), GFP_KERNEL);
+	if (!cport)
+		return ERR_PTR(-ENOMEM);
+
+	mutex_lock(&idr_lock);
+	ret = idr_alloc(&tty_idr, cport, 0, MAX_TTY_RPMSG, GFP_KERNEL);
+	mutex_unlock(&idr_lock);
+
+	if (ret < 0) {
+		kfree(cport);
+		return ERR_PTR(ret);
+	}
+
+	cport->id = ret;
+
+	return cport;
+}
+
+static void rpmsg_tty_destruct_port(struct tty_port *port)
+{
+	struct rpmsg_tty_port *cport = container_of(port, struct rpmsg_tty_port, port);
+
+	mutex_lock(&idr_lock);
+	idr_remove(&tty_idr, cport->id);
+	mutex_unlock(&idr_lock);
+
+	kfree(cport);
+}
+
+static const struct tty_port_operations rpmsg_tty_port_ops = {
+	.destruct = rpmsg_tty_destruct_port,
+};
+
 
 static int rpmsg_tty_probe(struct rpmsg_device *rpdev)
 {
-	struct tty_rpmsg_port *port;
-	struct device *dev;
-	u32 max_size;
+	struct rpmsg_tty_port *cport;
+	struct device *dev = &rpdev->dev;
+	struct device *tty_dev;
 	int ret;
 
-	port = devm_kzalloc(&rpdev->dev, sizeof(*port), GFP_KERNEL);
-	if (!port)
-		return -ENOMEM;
-	dev_set_drvdata(&rpdev->dev, &port->port);
+	cport = rpmsg_tty_alloc_cport();
+	if (IS_ERR(cport))
+		return dev_err_probe(dev, PTR_ERR(cport), "Failed to alloc tty port\n");
 
-	tty_port_init(&port->port);
-	port->port.ops = &tty_rpmsg_port_ops;
-	INIT_WORK(&port->work, tty_rpmsg_write_work);
+	tty_port_init(&cport->port);
+	cport->port.ops = &rpmsg_tty_port_ops;
 
-	/*
-	 * don't limit receive buffer size by default since:
-	 * 1.tty core don't notify us when somebody read the data from buffer
-	 * 2.it's hard to send RPMSG_TTY_WAKEUP once some space is available
-	 */
-	if (of_property_read_u32(rpdev->dev.of_node,
-				 "max-size", &max_size) < 0) {
-		max_size = INT_MAX;
-	}
-	tty_buffer_set_limit(&port->port, max_size);
-
-	port->driver = tty_alloc_driver(1, TTY_DRIVER_REAL_RAW |
-		TTY_DRIVER_DYNAMIC_DEV | TTY_DRIVER_UNNUMBERED_NODE);
-	if (IS_ERR(port->driver)) {
-		ret = PTR_ERR(port->driver);
-		goto put_port;
+	tty_dev = tty_port_register_device(&cport->port, rpmsg_tty_driver,
+					   cport->id, dev);
+	if (IS_ERR(tty_dev)) {
+		ret = dev_err_probe(dev, PTR_ERR(tty_dev), "Failed to register tty port\n");
+		tty_port_put(&cport->port);
+		return ret;
 	}
 
-	port->driver->driver_name  = rpdev->id.name;
-	port->driver->name         = rpdev->id.name + 6; /* skip "rpmsg-" */
-	port->driver->type         = TTY_DRIVER_TYPE_SERIAL |
-				     TTY_DRIVER_REAL_RAW;
-	port->driver->subtype      = SERIAL_TYPE_NORMAL;
+	cport->rpdev = rpdev;
 
-	port->driver->init_termios = tty_std_termios;
-	port->driver->init_termios.c_iflag = 0;
-	port->driver->init_termios.c_oflag = 0;
-	port->driver->init_termios.c_lflag = 0;
+	dev_set_drvdata(dev, cport);
 
-	tty_set_operations(port->driver, &tty_rpmsg_ops);
-	ret = tty_register_driver(port->driver);
-	if (ret < 0)
-		goto put_tty;
-
-	dev = tty_port_register_device_attr(&port->port, port->driver,
-					    0, &rpdev->dev, rpdev, NULL);
-	if (IS_ERR(dev)) {
-		ret = PTR_ERR(dev);
-		goto tty_unregister;
-	}
-
-	/* send announcement message for remote to know this address */
-	rpdev->announce = 1;
+	dev_dbg(dev, "New channel: 0x%x -> 0x%x: " RPMSG_TTY_NAME "%d\n",
+		rpdev->src, rpdev->dst, cport->id);
 
 	return 0;
-
-tty_unregister:
-	tty_unregister_driver(port->driver);
-put_tty:
-	tty_driver_kref_put(port->driver);
-put_port:
-	tty_port_put(&port->port);
-	return ret;
 }
 
 static void rpmsg_tty_remove(struct rpmsg_device *rpdev)
 {
-	struct tty_port *p = dev_get_drvdata(&rpdev->dev);
-	struct tty_rpmsg_port *port = to_tty_rpmsg_port(p);
+	struct rpmsg_tty_port *cport = dev_get_drvdata(&rpdev->dev);
 
-	cancel_work_sync(&port->work);
-	tty_unregister_device(port->driver, 0);
-	tty_unregister_driver(port->driver);
-	tty_driver_kref_put(port->driver);
-	tty_port_put(p);
+	dev_dbg(&rpdev->dev, "Removing rpmsg tty device %d\n", cport->id);
+
+	/* User hang up to release the tty */
+	tty_port_tty_hangup(&cport->port, false);
+
+	tty_unregister_device(rpmsg_tty_driver, cport->id);
+
+	tty_port_put(&cport->port);
 }
 
-static int rpmsg_tty_write_handler(struct rpmsg_device *rpdev,
-				   void *data, int len, void *priv, u32 src)
-{
-	struct tty_port *p = dev_get_drvdata(&rpdev->dev);
-	struct rpmsg_tty_write *msg = data;
-	int ret;
+static struct rpmsg_device_id rpmsg_driver_tty_id_table[] = {
+	{ .name	= "rpmsg-tty" },
+	{ },
+};
+MODULE_DEVICE_TABLE(rpmsg, rpmsg_driver_tty_id_table);
 
-	ret = tty_insert_flip_string(p, msg->data, msg->count);
-	if (ret > 0)
-		tty_flip_buffer_push(p);
-
-	msg->header.response = 1;
-	msg->header.result = ret;
-
-	return rpmsg_send(rpdev->ept, msg, sizeof(*msg));
-}
-
-static int rpmsg_tty_wakeup_handler(struct rpmsg_device *rpdev,
-				    void *data, int len, void *priv, u32 src)
-{
-	struct tty_port *p = dev_get_drvdata(&rpdev->dev);
-	struct tty_rpmsg_port *port = to_tty_rpmsg_port(p);
-
-	schedule_work(&port->work);
-	return 0;
-}
-
-static const rpmsg_rx_cb_t rpmsg_tty_handler[] = {
-	[RPMSG_TTY_WRITE]  = rpmsg_tty_write_handler,
-	[RPMSG_TTY_WAKEUP] = rpmsg_tty_wakeup_handler,
+static struct rpmsg_driver rpmsg_tty_rpmsg_drv = {
+	.drv.name	= KBUILD_MODNAME,
+	.id_table	= rpmsg_driver_tty_id_table,
+	.probe		= rpmsg_tty_probe,
+	.callback	= rpmsg_tty_cb,
+	.remove		= rpmsg_tty_remove,
 };
 
-static int rpmsg_tty_callback(struct rpmsg_device *rpdev,
-			      void *data, int len, void *priv, u32 src)
+static int __init rpmsg_tty_init(void)
 {
-	struct rpmsg_tty_header *hdr = data;
-	u32 cmd = hdr->command;
-	int ret = -EINVAL;
+	int ret;
 
-	if (hdr->response) {
-		struct rpmsg_tty_cookie *cookie =
-			(struct rpmsg_tty_cookie *)hdr->cookie;
+	rpmsg_tty_driver = tty_alloc_driver(MAX_TTY_RPMSG, TTY_DRIVER_REAL_RAW |
+					    TTY_DRIVER_DYNAMIC_DEV);
+	if (IS_ERR(rpmsg_tty_driver))
+		return PTR_ERR(rpmsg_tty_driver);
 
-		cookie->result = hdr->result;
-		complete(&cookie->done);
-	} else if (cmd < ARRAY_SIZE(rpmsg_tty_handler)) {
-		ret = rpmsg_tty_handler[cmd](rpdev, data, len, priv, src);
-	} else {
-		dev_err(&rpdev->dev, "invalid command %u\n", cmd);
+	rpmsg_tty_driver->driver_name = "rpmsg_tty";
+	rpmsg_tty_driver->name = RPMSG_TTY_NAME;
+	rpmsg_tty_driver->major = 0;
+	rpmsg_tty_driver->type = TTY_DRIVER_TYPE_CONSOLE;
+
+	/* Disable unused mode by default */
+	rpmsg_tty_driver->init_termios = tty_std_termios;
+	rpmsg_tty_driver->init_termios.c_lflag &= ~(ECHO | ICANON);
+	rpmsg_tty_driver->init_termios.c_oflag &= ~(OPOST | ONLCR);
+
+	tty_set_operations(rpmsg_tty_driver, &rpmsg_tty_ops);
+
+	ret = tty_register_driver(rpmsg_tty_driver);
+	if (ret < 0) {
+		pr_err("Couldn't install driver: %d\n", ret);
+		goto error_put;
 	}
+
+	ret = register_rpmsg_driver(&rpmsg_tty_rpmsg_drv);
+	if (ret < 0) {
+		pr_err("Couldn't register driver: %d\n", ret);
+		goto error_unregister;
+	}
+
+	return 0;
+
+error_unregister:
+	tty_unregister_driver(rpmsg_tty_driver);
+
+error_put:
+	tty_driver_kref_put(rpmsg_tty_driver);
 
 	return ret;
 }
 
-static int rpmsg_tty_match(struct rpmsg_device *dev, struct rpmsg_driver *drv)
+static void __exit rpmsg_tty_exit(void)
 {
-	const char *devname = dev->id.name;
-	const char *drvname = "rpmsg-tty";
-
-	/* match all device start with rpmsg-tty */
-	return strncmp(devname, drvname, strlen(drvname)) == 0;
+	unregister_rpmsg_driver(&rpmsg_tty_rpmsg_drv);
+	tty_unregister_driver(rpmsg_tty_driver);
+	tty_driver_kref_put(rpmsg_tty_driver);
+	idr_destroy(&tty_idr);
 }
 
-static struct rpmsg_driver rpmsg_tty_driver = {
-	.drv = {
-		.name	= "rpmsg_tty",
-		.owner	= THIS_MODULE,
-	},
-	.probe		= rpmsg_tty_probe,
-	.remove		= rpmsg_tty_remove,
-	.callback	= rpmsg_tty_callback,
-	.match		= rpmsg_tty_match,
-};
+module_init(rpmsg_tty_init);
+module_exit(rpmsg_tty_exit);
 
-module_rpmsg_driver(rpmsg_tty_driver);
-
-MODULE_ALIAS("rpmsg:rpmsg_tty");
-MODULE_AUTHOR("Xiang Xiao <xiaoxiang@pinecone.net>");
-MODULE_DESCRIPTION("TTY over the rpmsg channel");
+MODULE_AUTHOR("Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>");
+MODULE_DESCRIPTION("remote processor messaging tty driver");
 MODULE_LICENSE("GPL v2");
