d16e506cb9f21675d71a30b36a374405220f91a5
Peter van der Perk <peter.vanderperk@nxp.com>
2026-06-03 16:05:08 +0200

    navq95: rpmsg: zero-copy TX API and rpmsg_tty2
    (cherry picked from commit 68556c1d3e393dcee96d68375720f6d713949f46)
    (cherry picked from commit 6ece47ce296f93424c9cd01f492fbacafa1134ee)
    Co-authored-by: André Heinemans <andre.heinemans@nxp.com>

--- drivers/rpmsg/rpmsg_internal.h
+++ drivers/rpmsg/rpmsg_internal.h
@@ -47,7 +47,14 @@ struct rpmsg_device_ops {
 
 /**
  * struct rpmsg_endpoint_ops - indirection table for rpmsg_endpoint operations
- * @destroy_ept:	see @rpmsg_destroy_ept(), required
+ * @destroy_ept:		see @rpmsg_destroy_ept(), required
+ * @get_tx_buffer_size:		see @rpmsg_get_tx_buffer_size(), optional
+ * @get_rx_buffer_size:		see @rpmsg_get_rx_buffer_size(), optional
+ * @get_tx_payload_buffer:	see @rpmsg_get_tx_payload_buffer(), optional
+ * @release_tx_buffer:		see @rpmsg_release_tx_buffer(), optional
+ * @send_nocopy:		see @rpmsg_send_nocopy(), optional
+ * @sendto_nocopy:		see @rpmsg_sendto_nocopy(), optional
+ * @send_offchannel_nocopy:	see @rpmsg_send_offchannel_nocopy(), optional
  * @send:		see @rpmsg_send(), required
  * @sendto:		see @rpmsg_sendto(), optional
  * @trysend:		see @rpmsg_trysend(), required
@@ -62,6 +69,17 @@ struct rpmsg_device_ops {
  */
 struct rpmsg_endpoint_ops {
 	void (*destroy_ept)(struct rpmsg_endpoint *ept);
+
+	int (*get_tx_buffer_size)(struct rpmsg_endpoint *ept);
+    int (*get_rx_buffer_size)(struct rpmsg_endpoint *ept);
+
+       void *(*get_tx_payload_buffer)(struct rpmsg_endpoint *ept,
+                                      unsigned int *len, bool wait);
+       int (*release_tx_buffer)(struct rpmsg_endpoint *ept, void *txbuf);
+       int (*send_nocopy)(struct rpmsg_endpoint *ept, void *data, int len);
+       int (*sendto_nocopy)(struct rpmsg_endpoint *ept, void *data, int len, u32 dst);
+       int (*send_offchannel_nocopy)(struct rpmsg_endpoint *ept, u32 src, u32 dst,
+                                     void *data, int len);
 
 	int (*send)(struct rpmsg_endpoint *ept, void *data, int len);
 	int (*sendto)(struct rpmsg_endpoint *ept, void *data, int len, u32 dst);
--- include/linux/rpmsg.h
+++ include/linux/rpmsg.h
@@ -118,6 +118,7 @@ struct rpmsg_driver {
 	void (*remove)(struct rpmsg_device *dev);
 	int (*callback)(struct rpmsg_device *, void *, int, void *, u32);
 	int (*flowcontrol)(struct rpmsg_device *, void *, bool);
+	int (*match)(struct rpmsg_device *dev, const struct rpmsg_driver *drv);
 };
 
 static inline u16 rpmsg16_to_cpu(struct rpmsg_device *rpdev, __rpmsg16 val)
@@ -181,6 +182,16 @@ void rpmsg_destroy_ept(struct rpmsg_endpoint *);
 struct rpmsg_endpoint *rpmsg_create_ept(struct rpmsg_device *,
 					rpmsg_rx_cb_t cb, void *priv,
 					struct rpmsg_channel_info chinfo);
+int rpmsg_get_tx_buffer_size(struct rpmsg_endpoint *ept);
+int rpmsg_get_rx_buffer_size(struct rpmsg_endpoint *ept);
+void *rpmsg_get_tx_payload_buffer(struct rpmsg_endpoint *ept,
+                                 unsigned int *len, bool wait);
+int rpmsg_release_tx_buffer(struct rpmsg_endpoint *ept, void *txbuf);
+
+int rpmsg_send_nocopy(struct rpmsg_endpoint *ept, void *data, int len);
+int rpmsg_sendto_nocopy(struct rpmsg_endpoint *ept, void *data, int len, u32 dst);
+int rpmsg_send_offchannel_nocopy(struct rpmsg_endpoint *ept, u32 src, u32 dst,
+                                void *data, int len);
 
 int rpmsg_send(struct rpmsg_endpoint *ept, void *data, int len);
 int rpmsg_sendto(struct rpmsg_endpoint *ept, void *data, int len, u32 dst);
@@ -247,6 +258,64 @@ static inline struct rpmsg_endpoint *rpmsg_create_ept(struct rpmsg_device *rpdev
 	WARN_ON(1);
 
 	return NULL;
+}
+
+int rpmsg_get_tx_buffer_size(struct rpmsg_endpoint *ept)
+{
+       /* This shouldn't be possible */
+       WARN_ON(1);
+
+       return -ENXIO;
+}
+
+int rpmsg_get_rx_buffer_size(struct rpmsg_endpoint *ept)
+{
+       /* This shouldn't be possible */
+       WARN_ON(1);
+
+       return -ENXIO;
+}
+
+static inline void *rpmsg_get_tx_payload_buffer(struct rpmsg_endpoint *ept,
+                                               unsigned int *len, bool wait)
+{
+       /* This shouldn't be possible */
+       WARN_ON(1);
+
+       return ERR_PTR(-ENXIO);
+}
+
+static inline int rpmsg_release_tx_buffer(struct rpmsg_endpoint *ept, void *txbuf)
+{
+       /* This shouldn't be possible */
+       WARN_ON(1);
+
+       return -ENXIO;
+}
+
+static inline int rpmsg_send_nocopy(struct rpmsg_endpoint *ept, void *data, int len)
+{
+       /* This shouldn't be possible */
+       WARN_ON(1);
+
+       return -ENXIO;
+}
+
+static inline int rpmsg_sendto_nocopy(struct rpmsg_endpoint *ept, void *data, int len, u32 dst)
+{
+       /* This shouldn't be possible */
+       WARN_ON(1);
+
+       return -ENXIO;
+}
+
+static inline int rpmsg_send_offchannel_nocopy(struct rpmsg_endpoint *ept, u32 src, u32 dst,
+                                              void *data, int len)
+{
+       /* This shouldn't be possible */
+       WARN_ON(1);
+
+       return -ENXIO;
 }
 
 static inline int rpmsg_send(struct rpmsg_endpoint *ept, void *data, int len)
